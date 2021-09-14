0d4ae742f33eb9f4548495afa33d00500466fb06
intx <code@intralix.com>
2021-09-14 17:43:08 +0000

    Cleanup

--- odoo_library/models/.ipynb_checkpoints/book-checkpoint.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class Book(models.Model):
-    _name = 'library.book'
-    _description = 'Book of library'
--- odoo_volunteers/models/volunteer.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class Volunteers(models.Model):
-
-    _name = 'cooperative.volunteers'
-    _description = "Volunteers Info"
