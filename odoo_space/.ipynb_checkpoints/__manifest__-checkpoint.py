c49dfcf14d668c7c7390deb99cb410b5d08a1cb3
LuisIntralix <lrodriguez@intralix.com>
2021-09-14 17:54:33 +0000

    [FIX] delete trash files.

---
+++ odoo_space/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Space Mission',
+    'description': 'Module for go to moon!!',
+    'author': 'Intralix BI',
+    'website': 'https://www.intralix.com',
+    'version': '14.0.1',
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ]
+}
