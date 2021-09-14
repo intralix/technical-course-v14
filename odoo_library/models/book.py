f4d426a04ebc2f91b87452c5ad99da6eeb481998
Jonathan Rivera <jrivera@intralix.com>
2021-09-14 14:37:45 -0500

    Add fields

--- odoo_library/models/book.py
+++ odoo_library/models/book.py
@@ -5,3 +5,9 @@ from odoo import models, fields, api
 class Book(models.Model):
     _name = 'library.book'
     _description = 'Book of library'
+    titulo = fields.Char(string = 'Author',required = True)
+    name = fields.Char(string = 'BookName',required = True)
+    editores = fields.Char(string = 'Editor',required = True)
+    year = fields.Integer(string = 'Year',required = True)   
+    isbn = fields.Char(string = 'ISBN',required = True) 
+    genre = fields.Char(string = 'Genre',Required = True)
