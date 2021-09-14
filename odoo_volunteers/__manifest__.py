89582075e799c7397364b02e6ce93e7b993fb8fb
ivanintralix <operez@intralix.com>
2021-09-14 00:06:27 -0500

    [ADD] Volunteers Module
    [ADD] Volunteers Module

---
+++ odoo_volunteers/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Odoo Cooperative Volunteers',
+    'description' : 'Cooperative Volunteers',
+    'author' : 'Intralix BI',
+    'website' : 'https://www.intralix.com',
+    'version' : '14.0.1',
+    'depends' : ['base'],
+    'data' : [
+        
+    ],
+     'demo' : [
+         
+     ]  
+}
