2fd0cf5bc96e98e050ffa1ecfff8b729100f8e0f
JLAM <code@intralix.com>
2021-09-13 23:24:23 -0500

    [ADD] Course Models

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Odoo Academy',
+    'summary' : 'Academy app to manage Trainig',
+    'description' : """Academy Module to manage Training
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    'author' : 'Javina',
+    'website' : 'https://www.odoo.com',
+    'category' : 'training',
+    'version' : '0.0.1',
+    'depends' : ['base'],
+    'data' : [
+        
+    ],
+    
+    'demo' : [
+        
+    ],
+}
