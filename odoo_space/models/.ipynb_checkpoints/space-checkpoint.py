9620f4be81882b2335bc0bd30038c659af5cd450
LuisIntralix <lrodriguez@intralix.com>
2021-09-14 20:30:56 +0000

    [FIX] Add new fields and fix model.

--- odoo_space/models/.ipynb_checkpoints/space-checkpoint.py
+++ odoo_space/models/.ipynb_checkpoints/space-checkpoint.py
@@ -4,17 +4,31 @@ from odoo import models, fields, api
 
 class Space(models.Model):
     _name = 'space.space'
-    _description = 'Information About Space'
+    _description = 'Information About spaceShips'
 
     name = fields.Char(string='Title',required=True)
+    
     description = fields.Text(string='Description')
     
-    level = fields.Selection(string='Level',
+    width = fields.Integer(string='Width',required=True)
+    height = fields.Integer(string='Height',required=True)
+    
+    fueltype = fields.Selection(string='fuelType',
                             selection=[
-                                ('beginner','Beginner'),
-                                ('intermediate','Intermediate'),
-                                ('advanced','Advanced')
+                                ('diesel','Diesel'),
+                                ('gas','Gas'),
+                                ('volts','Volts')
                                      ],
                              copy=False)
+    
+    shiptype = fields.Selection(string='shipType',
+                            selection=[
+                                ('moenix','Moenix'),
+                                ('goliath','Goliath'),
+                                ('perum','Perum')
+                                     ],
+                             copy=False)
+    
+    passengers = fields.Integer(string='Passengers',required=False)
     
     active = fields.Boolean(string='Active',default=True)
