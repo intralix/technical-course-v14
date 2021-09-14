8de2eeed1b33cb701372697b2766f40fb61fcca8
ivan <operez@intralix.com>
2021-09-14 12:26:50 -0500

    name model
    name model

--- odoo_volunteers/models/__init__.py
+++ odoo_volunteers/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import volunteer
+from . import task
--- odoo_volunteers/models/task.py
+++ odoo_volunteers/models/task.py
@@ -2,7 +2,7 @@
 
 from odoo import models, fields, api
 
-class Task(models.Model):
+class task(models.Model):
 
     _name = 'cooperative.volunteers'
     _description = 'Volunteers Info'
