1e4ec3b3577ac8420aebc0156919ae07e679740d
JRiveraDiaz <jrivera@intralix.com>
2021-09-15 17:47:07 +0000

    [ADD] fix 3

---
+++ odoo_library/models/.ipynb_checkpoints/book-checkpoint.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Book(models.Model):
+    _name = 'library.book'
+    _description = 'Book of library'
+    titulo = fields.Char(string = 'Author',required = True)
+    name = fields.Char(string = 'BookName',required = True)
+    editores = fields.Char(string = 'Editor',required = True)
+    year = fields.Integer(string = 'Year',required = True)   
+    isbn = fields.Char(string = 'ISBN',required = True) 
+    genre = fields.Char(string = 'Genre',required = True)
+    notes = fields.Text(string = 'notes',required = True)
