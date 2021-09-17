056d0718d08be4ec929496a22074d4c2263d0f9d
Jonathan Rivera <jrivera@intralix.com>
2021-09-17 11:59:09 -0500

    Technical Training: ORM Advanced fix 1

--- odoo_library/models/book.py
+++ odoo_library/models/book.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from odoo.Exceptions import UserError,ValidationError
+from odoo.exceptions import UserError,ValidationError
 
 class Book(models.Model):
     _name = 'library.book'
