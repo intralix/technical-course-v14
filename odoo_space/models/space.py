f3f5d95f385cf1d43dc22983c5e846f519e0b6ac
Luis Eduardo Rodriguez <lrodriguez@intralix.com>
2021-09-17 16:01:37 +0000

    [ADD] New validation in size of spaceship

--- odoo_space/models/space.py
+++ odoo_space/models/space.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Space(models.Model):
     _name = 'space.space'
@@ -31,3 +32,22 @@ class Space(models.Model):
     passengers = fields.Integer(string='Pasajeros',required=False)
     
     active = fields.Boolean(string='Active')
+
+    base_price = fields.Float(string='Base Price', default=0.00)
+
+    additional_fee = fields.Float(string='Additional Fee',default=0.00)
+
+    total_price = fields.Float(string='Total price',readonly=True)
+
+    @api.onchange('base_price','additional_fee')
+    def _onchange_total_price(self):
+        if self.base_price < 0.00:
+            raise UserError('Base Price cannot be set as Negative')
+
+        self.total_price = self.base_price + self.additional_fee
+
+    @api.constrains('additional_fee')
+    def _check_additional_fee(self):
+        for record in self:
+            if record.additional_fee < 10.00:
+                raise ValidationError('Additional Fees cannot be less than 10.10: %s' % record.additional_fee) 
