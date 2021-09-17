248e143506ce5b8cc8acd93b96f48649efe6757a
Jonathan Rivera <jrivera@intralix.com>
2021-09-17 11:55:52 -0500

    Technical Training: ORM Advanced

--- odoo_library/models/book.py
+++ odoo_library/models/book.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.Exceptions import UserError,ValidationError
 
 class Book(models.Model):
     _name = 'library.book'
@@ -12,3 +13,8 @@ class Book(models.Model):
     isbn = fields.Char(string = 'ISBN',required = True) 
     genre = fields.Char(string = 'Genre',required = True)
     notes = fields.Text(string = 'notes',required = True)
+
+    @api.onchange('isbn')
+    def _onchange_length_isbn(self):
+        if len(self.isbn) < 13:
+            raise UserError('The length of the ISBN cannot be less than 13 characters')
