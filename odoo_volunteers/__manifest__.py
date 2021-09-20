40de07140d0c15fcb87c5a47d510c393ae816b49
ivanintralix <77854285+ivanintralix@users.noreply.github.com>
2021-09-19 23:23:55 -0500

    [ADD] many2many field and gant view
    [ADD] many2many field and gant view

--- odoo_volunteers/__manifest__.py
+++ odoo_volunteers/__manifest__.py
@@ -9,6 +9,7 @@
     'category' : 'training',
     'depends' : ['base'],
     'data' : [
+        'security/volunteers_security.xml',
         'security/ir.model.access.csv',
         'views/volunteers_views.xml',
         'views/volunteers_menuitems.xml'
