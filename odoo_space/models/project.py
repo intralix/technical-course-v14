c8966c9ef616521de09a51d84a90ac6fd218e013
Luis Eduardo Rodriguez <lrodriguez@intralix.com>
2021-09-22 16:58:12 +0000

    [FIX] Fix project module. r3

--- odoo_space/models/project.py
+++ odoo_space/models/project.py
@@ -5,4 +5,5 @@ from odoo import models, fields, api
 class Project(models.Model):
     _inherit = 'project.project'
 
-    mission_id = fields.Many2many(comodel_name='space.mission', string='Misiones de un proyecto')
+    mission_id = fields.Many2many(comodel_name='space.mission', string='Misión del proyecto')
+
