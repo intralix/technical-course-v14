a17d82cd34442b4650c145366a64b8156f39f58a
intx <code@intralix.com>
2021-09-23 16:38:43 +0000

    [ADD] Report

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -21,7 +21,8 @@
         'views/session_views.xml',
         'views/sales_view_inherit.xml',
         'views/product_views_inherit.xml',
-        'wizard/sale_wizard_view.xml'
+        'wizard/sale_wizard_view.xml',
+        'report/session_report_template.xml'
     ],
     
     'demo' : [
