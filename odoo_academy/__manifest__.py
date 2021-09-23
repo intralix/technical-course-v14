0be3caad886260d6da511fb5e669788960364121
intx <code@intralix.com>
2021-09-23 17:51:10 +0000

    [ADD] Web controller

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -10,8 +10,8 @@
     'author' : 'Javina',
     'website' : 'https://www.odoo.com',
     'category' : 'training',
-    'version' : '0.0.2',
-    'depends' : ['sale'],
+    'version' : '0.0.3',
+    'depends' : ['base','sale','website'],
     
     'data' : [
         'security/academy_security.xml',
@@ -22,7 +22,8 @@
         'views/sales_view_inherit.xml',
         'views/product_views_inherit.xml',
         'wizard/sale_wizard_view.xml',
-        'report/session_report_template.xml'
+        'report/session_report_template.xml',
+        'views/academy_web_templates.xml'
     ],
     
     'demo' : [
