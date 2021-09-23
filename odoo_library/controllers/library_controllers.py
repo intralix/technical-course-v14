d06a4130f2909c06e4508a6bd7d33e40cbc15373
Jonathan Rivera <jrivera@intralix.com>
2021-09-23 14:22:20 -0500

    Technical Training: Controllers fix1

--- odoo_library/controllers/library_controllers.py
+++ odoo_library/controllers/library_controllers.py
@@ -3,13 +3,13 @@ from odoo import http
 
 class Library(http.Controller):
     @http.route('/library/',auth='public',website=True)
-    def  index(self,**kw):
+    def index(self,**kw):
         return "Hello,world"
 
     @http.route('/library/books/',auth='public',website=True)
     def books(self,**kw):
         books = http.request.env['library.book'].search([])
-        return http.request.render('library.book_website',{
+        return http.request.render('odoo_library.book_website',{
             'books':books
         })
 
